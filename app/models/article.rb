a71d320993c2e83533f693acc5e5b3851117ebc5
Eufranor Filho <eufranorfilho.dev@gmail.com>
2024-06-26 22:58:53 -0300

    #4 - Conexão ao banco de dados, desenvolvimento de novas rotas e ajustes de caminhos por meio de botões no blog

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+    validates :title, presence: true, length: 3..100
+    validates :author, presence: true, length: 2..20
+    validates :category, presence: true, length: 2..20
+    validates :content, presence: true, length: 10..1000
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+    # Defines the root path route ("/")
+  root "blog#index"
+  get 'blog/articles/:id', to: 'blog#articles', as: 'blog_article'
 end
